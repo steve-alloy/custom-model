f8cac0fda918aaaa36fc58931a2dfef96587f1b9
steve-alloy <stevegiordano@Steves-MacBook-Pro.local>
2022-02-10 10:21:01 -0500

    Fixing_method

--- index.ts
+++ index.ts
@@ -8,8 +8,7 @@ const port: string | number = process.env.PORT || 3000;
 const app: Application = express();
 
 app.post("/", (req: Request, res: Response) => {
-    console.log("METHOD IS POST");
-    console.log(req);
+    console.log(req.body);
     res.json({
         "modelSuccess": true,
         "modelScore": 123,
@@ -17,35 +16,6 @@ app.post("/", (req: Request, res: Response) => {
     })
 });
 
-app.put("/", (req: Request, res: Response) => {
-    console.log("METHOD IS PUT");
-    console.log(req);
-    res.json({
-        "modelSuccess": true,
-        "modelScore": 123,
-        "modelVersion": 1
-    })
-});
-
-app.patch("/", (req: Request, res: Response) => {
-    console.log("METHOD IS PATCH");
-    console.log(req);
-    res.json({
-        "modelSuccess": true,
-        "modelScore": 123,
-        "modelVersion": 1
-    })
-});
-
-app.get("/", (req: Request, res: Response) => {
-    console.log("METHOD IS GET");
-    console.log(req);
-    res.json({
-        "modelSuccess": true,
-        "modelScore": 123,
-        "modelVersion": 1
-    })
-});
 app.listen(port, () => {
     console.log(`Listening on port ${port}`);
 });
